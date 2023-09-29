fc5018f9ca7fd98f02b76b93a618dc3929c1afb1
Logboy2000 <loganhowarth@icloud.com>
2023-09-29 13:23:33 -0600

    Create Led_Project.ino
    ardweeno

---
+++ Structured Programming 2/Led_Project/Led_Project.ino
@@ -0,0 +1,34 @@
+//Declare Global Variables
+int ledRed = 6;
+int ledYellow = 5;
+int redBrightness = 0;
+int yellowBrightness = 0;
+
+void setup() {
+  //set pin mode
+  pinMode (ledRed, OUTPUT);
+  pinMode (ledYellow, OUTPUT);
+  //set serial
+  Serial.begin(9600);
+}//end of setup
+
+void loop() {
+  //updates the variables
+  analogWrite (ledRed, redBrightness);
+  analogWrite (ledYellow, yellowBrightness);
+
+  //changes the brightness values
+  redBrightness = redBrightness + 10;
+  yellowBrightness = yellowBrightness - 10;
+  
+
+
+  //prints brightness values
+  Serial.println(redBrightness);
+  Serial.println("Red Brightness:");
+  Serial.println(yellowBrightness);
+  Serial.println("Yellow Bightness:");
+
+  //delays the loop by 250ms
+  delay(250);
+}//end of loop
